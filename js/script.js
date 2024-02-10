b2534160d5c2c980027764993c11ffd30e129c92
DimaSurdiy <dsurdiy@ukr.net>
2024-02-10 13:13:44 +0200

    added rating

--- js/script.js
+++ js/script.js
@@ -18,7 +18,51 @@ function documentActions(e) {
       }
       slideToggle(currentElement.nextElementSibling);
    }
+
+   if (targetElement.closest(".rating__input")) {
+      const currentElement = targetElement.closest(".rating__input");
+      const rating = currentElement.closest(".rating");
+      if (rating.classList.contains("rating--set")) {
+         starRatingGet(rating, currentElement);
+      }
+   }
 }
+
+// Rating
+const ratings = document.querySelectorAll("[data-rating]");
+if (ratings) {
+   ratings.forEach(rating => {
+      const currentValue = +rating.dataset.rating;
+      currentValue ? starRatingSet(rating, currentValue) : null;
+   });
+}
+
+function starRatingGet(rating, currentElement) {
+   const ratingValue = +currentElement.value;
+   // Тут відправка оцінки ratingValue на бекенд...
+   // уявімо, що ми отримали середню оцінку 3.2
+   const resultRating = 3.2;
+   starRatingSet(rating, resultRating);
+}
+
+function starRatingSet(rating, value) {
+   const ratingItems = rating.querySelectorAll(".rating__item");
+   const resultFullItems = parseInt(value);
+   const resultPartItem = value - resultFullItems;
+
+   ratingItems.forEach((ratingItem, index) => {
+      ratingItem.classList.remove("active");
+      ratingItem.querySelector("span") ? ratingItems[index].querySelector("span").remove() : null;
+
+      if (index <= (resultFullItems - 1)) {
+         ratingItem.classList.add("active");
+      }
+      if (index === resultFullItems && resultPartItem) {
+         ratingItem.insertAdjacentHTML("beforeend", `<span style="width:${resultPartItem * 100}%"></span>`);
+      }
+   })
+}
+
 
 // Спойлер в футері:
 const spollers = document.querySelectorAll("[data-spoller]");
